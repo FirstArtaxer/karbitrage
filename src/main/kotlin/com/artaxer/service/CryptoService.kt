e5b26221d08a9da6f9a223b8ff35d0e38a43ce0b
Ardeshir Ahouri <ardeshir.ahouri@gmail.com>
2024-06-20 19:13:30 +0330

    feat: Add ContentEncoding into CIO and change package.

---
+++ src/main/kotlin/com/artaxer/service/CryptoService.kt
@@ -0,0 +1,75 @@
+package com.artaxer.service
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.serializers.LocalDateTimeIso8601Serializer
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+
+@Serializable
+data class CryptoDto(
+    val code: String,
+    val price: Double,
+    val exchange: String,
+    @Serializable(with = LocalDateTimeIso8601Serializer::class)
+    val dateTime: LocalDateTime
+)
+
+class CryptoService(private val database: Database) {
+
+    object CryptoEntity : Table() {
+        val id = integer("id").autoIncrement()
+        val exchange = varchar("exchange", length = 100)
+        val prices = varchar("price", length = 255)
+        val dateTime = datetime("dateTime")
+        override var primaryKey = PrimaryKey(id)
+    }
+    init {
+        transaction(database) {
+            SchemaUtils.create(CryptoEntity)
+        }
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    suspend fun save(exchangePrices: Triple<String, String, LocalDateTime>) = dbQuery {
+        CryptoEntity.insert {
+            it[exchange] = exchangePrices.first
+            it[prices] = exchangePrices.second
+            it[dateTime] = exchangePrices.third
+        }[CryptoEntity.id]
+    }
+
+    suspend fun getHistoryPrices(fromDateTime: LocalDateTime): List<CryptoDto> {
+        return dbQuery {
+            CryptoEntity.select { CryptoEntity.dateTime greaterEq fromDateTime }
+                .flatMap {
+                    toCryptoDtoList(
+                        exchangeName = it[CryptoEntity.exchange],
+                        rawPrices = it[CryptoEntity.prices],
+                        dateTime = it[CryptoEntity.dateTime]
+                    )
+                }
+        }
+    }
+    private fun toCryptoDtoList(
+        exchangeName: String,
+        rawPrices: String,
+        dateTime: LocalDateTime
+    ): List<CryptoDto> {
+        return rawPrices.split(",")
+            .map {
+                val codeAndPrice = it.split("=")
+                CryptoDto(
+                    code = codeAndPrice.first(),
+                    price = codeAndPrice.last().toDouble(),
+                    exchange = exchangeName,
+                    dateTime = dateTime
+                )
+            }
+    }
+}
--- src/main/kotlin/com/artaxer/service/PriceService.kt
+++ src/main/kotlin/com/artaxer/service/PriceService.kt
@@ -3,11 +3,16 @@ package com.artaxer.service
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.compression.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 class PriceService {
-    private val client = HttpClient(CIO)
+    private val client = HttpClient(CIO) {
+        install(ContentEncoding) {
+            gzip()
+        }
+    }
     suspend fun getPrices(
         httpRequest: HttpRequestBuilder,
         extractor: (String) -> Map<String, Double>
